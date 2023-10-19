949cb19462fb9b04c520975f39c705668d0ce1f0
sakhawat71 <sakhawat.showrav@gmail.com>
2023-10-20 01:56:40 +0600

    calculetTriangleArea complete

--- app.js
+++ app.js
@@ -1,3 +1,24 @@
-const calculetTriangleArea = () =>{
-    console.log('u click nigga')
+const getValueFormInput = (inputId) => {
+    const inputfield = document.getElementById(inputId);
+    const inputValue = inputfield.value;
+    const input = parseFloat(inputValue);
+    return input;
 }
+const showArea = (id,area) =>{
+    const areaShow = document.getElementById(id);
+    areaShow.innerText = area;
+}
+
+
+
+const calculetTriangleArea = (input1, input2,showAreaId) => {
+    const base = getValueFormInput(input1);
+    const height = getValueFormInput(input2);
+    const area = 0.5 * base * height;
+
+    showArea(showAreaId,area)
+
+}
+
+
+
