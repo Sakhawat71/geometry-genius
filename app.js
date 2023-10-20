add34bccd1203d323c65be6e33a68caec0b312d7
sakhawat71 <sakhawat.showrav@gmail.com>
2023-10-20 12:11:51 +0600

    math complete

--- app.js
+++ app.js
@@ -5,66 +5,88 @@ const getValueFormInput = (inputId) => {
     inputfield.value = '';
     return input;
 }
-const showArea = (id,area) =>{
+const showArea = (id, area) => {
     const areaShow = document.getElementById(id);
     areaShow.innerText = area;
 }
 
+const checkPositiveNumber = (perameter1, perameter2) => {
+    if (isNaN(perameter1) || isNaN(perameter2) || perameter1 <= 0 || perameter2 <= 0) {
+        alert("Please insert positive numbers for both base and height.");
+        return false;
+    }
+    return true;
+}
 
 // Triangle
 const calculetTriangleArea = (baseId, heigtId, showAreaId) => {
     const base = getValueFormInput(baseId);
     const height = getValueFormInput(heigtId);
-    const area = 0.5 * base * height;
 
-    showArea(showAreaId,area)
+    if(checkPositiveNumber(base,height)){
+        const area = 0.5 * base * height;
+        showArea(showAreaId, area)
+    }
+      
 }
 
 // Rectangle
-const calculetRectangleArea = (widthId, heightId, showAreaId) =>{
+const calculetRectangleArea = (widthId, heightId, showAreaId) => {
     const width = getValueFormInput(widthId);
     const height = getValueFormInput(heightId);
-    const area = width * height;
-    showArea(showAreaId,area)
+
+    if(checkPositiveNumber(width,height)){
+        const area = width * height;
+        showArea(showAreaId, area)
+    }
 }
 
 // Parallelogram
 
-const calculetParallelogramArea = (baseID,heigtId,showAreaId) =>{
+const calculetParallelogramArea = (baseID, heigtId, showAreaId) => {
     const base = getValueFormInput(baseID);
     const height = getValueFormInput(heigtId);
 
-    const area = base * height;
-    showArea(showAreaId,area)
+    if(checkPositiveNumber(base,height)){
+        const area = base * height;
+        showArea(showAreaId, area)
+    }
 }
 
 // Rhombus
 
-const calculetRhombusArea = (diagonalAId,diagonalBId,showAreaId) =>{
+const calculetRhombusArea = (diagonalAId, diagonalBId, showAreaId) => {
     const diagonalA = getValueFormInput(diagonalAId);
     const diagonalB = getValueFormInput(diagonalBId);
-    
-    const area = diagonalA * diagonalB;
-    showArea(showAreaId,area)
+
+    if(checkPositiveNumber(diagonalA,diagonalB)){
+        const area = diagonalA * diagonalB;
+        showArea(showAreaId, area)
+    }
 }
 
 // Pentagon
 
-const calculetPentagonArea = (pentagonId,apothemId,showAreaId) => {
+const calculetPentagonArea = (pentagonId, apothemId, showAreaId) => {
     const pentagon = getValueFormInput(pentagonId);
     const apothem = getValueFormInput(apothemId)
-    const area = 0.5 * pentagon * apothem;
-    showArea(showAreaId,area)
+
+    if(checkPositiveNumber(pentagon,apothem)){
+        const area = 0.5 * pentagon * apothem;
+        showArea(showAreaId, area)
+    }
 }
 
 // Ellipse
 
-const calculetEllipseArea = (AId,BId,showAreaId) =>{
+const calculetEllipseArea = (AId, BId, showAreaId) => {
     const A = getValueFormInput(AId);
     const B = getValueFormInput(BId);
     const piRow = Math.PI;
     const pi = piRow.toFixed(2)
 
-    const area =  pi  * A * B;
-    showArea(showAreaId,area)
+    if(checkPositiveNumber(A,B)){
+        const area = pi * A * B;
+        showArea(showAreaId, area)
+    }
 }
